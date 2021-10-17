e6859906d252160124ac295558a6987e993d8f5f
trapperboy <92577580+trapperboy@users.noreply.github.com>
2021-10-17 14:52:16 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 JOIN CHANNAL 🎖', url=/n'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 JOIN CHANNAL 🎖', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
